b0a7b78d2af89e045c81fa6a452bd46df34c1dcb
Ewald Horn <ewaldhorn@gmail.com>
2020-02-27 17:37:33 +0200

    Doing some Kotlin OpenGL experimentation.

---
+++ android/JustSomeGel/app/src/main/java/za/co/nofuss/justsomegel/MainActivity.kt
@@ -0,0 +1,12 @@
+package za.co.nofuss.justsomegel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
