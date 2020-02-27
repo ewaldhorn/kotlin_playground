a301e5a9a409e8215b4fbe675bd30adec9ac797a
Ewald Horn <ewaldhorn@gmail.com>
2020-02-27 17:04:12 +0200

    Fixing things up a bit.  Mojave issues with ncurses.

--- SnakeGame/src/macosMain/kotlin/snake/SnakeGameMacos.kt
+++ SnakeGame/src/macosMain/kotlin/snake/SnakeGameMacos.kt
@@ -1,11 +1,15 @@
 package snake
 
 import platform.darwin.box
+import platform.darwin.curs_set
 import platform.darwin.delwin
 import platform.darwin.endwin
+import platform.darwin.halfdelay
 import platform.darwin.initscr
 import platform.darwin.mvwprintw
 import platform.darwin.newwin
+import platform.darwin.noecho
+import platform.darwin.wclear
 import platform.darwin.wgetch
 import platform.darwin.wrefresh
 
@@ -14,20 +18,40 @@ val windowHeight = 10
 
 fun main() {
     initscr()
+    noecho()
+    curs_set(0)
+    halfdelay(2)
 
-    val window = newwin(windowHeight, windowWidth, 0, 0)
-    box(window, 0, 0)
-    mvwprintw(window, 3, 2, "ooooQ")
-    wrefresh(window)
+    val window = newwin(windowHeight + 2, windowWidth + 2, 0, 0)
+    var snake = Snake(cells = listOf(Cell(2,0), Cell(1, 0), Cell(0,0)), direction = Direction.right)
 
-    wgetch(window)
-    wgetch(window)
+    var input = 0
+    while (input.toChar() != 'q') {
+        wclear(window)
+
+        box(window, 0, 0)
+
+        snake.tail.forEach {
+            mvwprintw(window, it.y + 1, it.x + 1, "o")
+        }
+
+        snake.head.let {mvwprintw(window, it.y + 1, it.x + 1, "Q")}
+
+        wrefresh(window)
+
+        input = wgetch(window)
+
+        snake = snake.move()
+    }
 
     delwin(window)
     endwin()
 }
 
 data class Snake(val cells: List<Cell>, val direction: Direction) {
+    val head = cells.first()
+    val tail = cells.subList(1, cells.size)
+
     fun move(): Snake {
         val newHead = cells.first().move(direction)
         val newTail = cells.dropLast(1)
