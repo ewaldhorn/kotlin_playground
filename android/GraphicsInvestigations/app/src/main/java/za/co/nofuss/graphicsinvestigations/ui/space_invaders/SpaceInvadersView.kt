ea7bacc167377ead188ef36fd316033bc377fae0
Ewald Horn <ewaldhorn@gmail.com>
2020-02-28 09:46:22 +0200

    Adjusting text size manually for now.

--- android/GraphicsInvestigations/app/src/main/java/za/co/nofuss/graphicsinvestigations/ui/space_invaders/SpaceInvadersView.kt
+++ android/GraphicsInvestigations/app/src/main/java/za/co/nofuss/graphicsinvestigations/ui/space_invaders/SpaceInvadersView.kt
@@ -91,7 +91,7 @@ class SpaceInvadersView(context: Context, private val size: Point) : SurfaceView
             // Draw the score and remaining lives
             // Change the brush color
             paint.color = Color.argb(255, 255, 255, 255)
-            paint.textSize = 70f
+            paint.textSize = 40f
             canvas.drawText(
                 "Score: $score   Lives: $lives Wave: " +
                     "$waves HI: $highScore", 20f, 75f, paint
