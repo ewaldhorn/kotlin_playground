94ed741ae3db5036cb7be4bf49f2956fbfac2f26
Ewald Horn <ewaldhorn@gmail.com>
2020-03-04 16:37:41 +0200

    More working on KOANS and some testing code.

---
+++ GradleJavaKotlinInterop/src/test/kotlin/date/DateTests.kt
@@ -0,0 +1,13 @@
+package date
+
+import org.junit.*
+import org.junit.Assert.assertEquals
+
+class DateTests {
+    @Test
+    fun `test that one date is equal to another`() {
+        val expected = true
+        val actual = MyDate(1, 1, 1) == MyDate(1, 1, 1)
+        assertEquals(expected, actual)
+    }
+}
