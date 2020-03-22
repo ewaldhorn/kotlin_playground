fa1abcce94e7a0df8c454bf9f5ee5cac1dd45d96
Ewald Horn <ewaldhorn@gmail.com>
2020-03-22 13:47:03 +0200

    Moving the Google Codelab stuff around so I can work on some other ideas in the same base project.

--- android/Posing/app/src/main/java/za/co/nofuss/posing/MainActivity.kt
+++ android/Posing/app/src/main/java/za/co/nofuss/posing/MainActivity.kt
@@ -3,25 +3,10 @@ package za.co.nofuss.posing
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Text
 import androidx.ui.core.setContent
-import androidx.ui.foundation.Border
-import androidx.ui.graphics.Color
-import androidx.ui.layout.Column
-import androidx.ui.layout.LayoutAlign
-import androidx.ui.layout.LayoutHeight
-import androidx.ui.layout.LayoutPadding
-import androidx.ui.layout.LayoutWidth
-import androidx.ui.layout.Row
-import androidx.ui.material.Button
-import androidx.ui.material.Divider
-import androidx.ui.material.MaterialTheme
-import androidx.ui.material.Surface
 import androidx.ui.tooling.preview.Preview
-import androidx.ui.unit.dp
-import androidx.ui.unit.sp
-import za.co.nofuss.posing.models.CounterState
-import za.co.nofuss.posing.theme.MyAppTheme
+import za.co.nofuss.posing.codelab.MyApp
+import za.co.nofuss.posing.codelab.MyScreenContent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,50 +16,6 @@ class MainActivity : AppCompatActivity() {
                 MyScreenContent()
             }
         }
-    }
-}
-
-@Composable
-fun MyApp(children: @Composable() () -> Unit) {
-    MyAppTheme {
-        Surface(color = Color.Gray, border = Border(1.dp, Color.Black)) {
-            children()
-        }
-    }
-}
-
-@Composable
-fun MyScreenContent(
-    names: List<String> = listOf("Android", "Again"),
-    counterState: CounterState = CounterState()
-) {
-    Column(modifier = LayoutHeight.Fill) {
-        Column(modifier = LayoutWeight(1f)) {
-            for (name in names) {
-                Greeting(name = name)
-                Divider(color = Color.Black)
-            }
-            Divider(color = Color.Transparent, height = 32.dp)
-        }
-        Row(modifier = LayoutWidth.Fill + LayoutAlign.CenterHorizontally) {
-            Counter(state = counterState)
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(
-        text = "Hello $name!",
-        style = MaterialTheme.typography().h1.copy(fontSize = 30.sp),
-        modifier = LayoutPadding(24.dp) + LayoutAlign.CenterHorizontally
-    )
-}
-
-@Composable
-fun Counter(state: CounterState) {
-    Button(onClick = { state.count++ }, modifier = LayoutPadding(bottom = 10.dp)) {
-        Text(text = "Button clicks: ${state.count}")
     }
 }
 
