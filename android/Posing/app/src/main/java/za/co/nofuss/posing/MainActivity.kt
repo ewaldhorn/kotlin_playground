06b62bc0bba81f9eedb1664373114f978b316de6
Ewald Horn <ewaldhorn@gmail.com>
2020-03-22 11:57:33 +0200

    Have the basic app building and running with Composable functions.

--- android/Posing/app/src/main/java/za/co/nofuss/posing/MainActivity.kt
+++ android/Posing/app/src/main/java/za/co/nofuss/posing/MainActivity.kt
@@ -5,29 +5,43 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.foundation.Border
+import androidx.ui.graphics.Color
+import androidx.ui.layout.LayoutAlign
+import androidx.ui.layout.LayoutPadding
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.Surface
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Greeting("Android")
-            }
+            MyApp()
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    MaterialTheme {
+        Surface(color = Color.Gray, border = Border(1.dp, Color.Black)) {
+            Greeting(name = "Android")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        modifier = LayoutPadding(24.dp) + LayoutAlign.CenterHorizontally
+    )
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
-    }
+    MyApp()
 }
