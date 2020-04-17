244b8d7d2a1a89d15c0abe624fd179d116e3d369
Ewald Horn <ewaldhorn@gmail.com>
2020-04-17 15:37:05 +0200

    Working through the Kotlin React tutorial at https://play.kotlinlang.org/hands-on/Building%20Web%20Applications%20with%20React%20and%20Kotlin%20JS/03_A_First_Static_Page

--- Web/kotlin_web_app/src/main/kotlin/App.kt
+++ Web/kotlin_web_app/src/main/kotlin/App.kt
@@ -1,3 +1,39 @@
+import models.*
+import react.dom.*
+import kotlin.browser.*
+
 fun main() {
-    console.log("Hello, Kotlin/JS!")
+    render(document.getElementById("root")) {
+        h1 {
+            +"Kotlin and React"
+        }
+        div {
+            h3 {
+                +"Videos to Watch"
+            }
+            for (video in unwatchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
+            }
+            h3 {
+                +"Videos watched"
+            }
+            for (video in watchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
+            }
+        }
+        div {
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                attrs {
+                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                }
+            }
+        }
+    }
 }
