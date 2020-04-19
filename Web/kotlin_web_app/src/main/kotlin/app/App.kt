accee56e44dac60a881a3be3321fb5a7f0d6c839
Ewald Horn <ewaldhorn@gmail.com>
2020-04-19 11:49:12 +0200

    Load videos from API using coroutines asynchronously.

--- Web/kotlin_web_app/src/main/kotlin/app/App.kt
+++ Web/kotlin_web_app/src/main/kotlin/app/App.kt
@@ -1,20 +1,24 @@
 package app
 
 import components.*
+import kotlinx.coroutines.*
 import models.*
 import react.*
 import react.dom.*
+import kotlin.browser.*
 
 class App : RComponent<RProps, AppState>() {
     override fun AppState.init() {
-        unwatchedVideos = listOf(
-            Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
-            Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
-            Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
-        )
-        watchedVideos = listOf(
-            Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
-        )
+        unwatchedVideos = listOf()
+        watchedVideos = listOf()
+
+        val mainScope = MainScope()
+        mainScope.launch {
+            val videos = fetchVideos()
+            setState {
+                unwatchedVideos = videos
+            }
+        }
     }
 
     override fun RBuilder.render() {
@@ -73,4 +77,30 @@ external interface AppState : RState {
     var currentVideo: Video?
     var unwatchedVideos: List<Video>
     var watchedVideos: List<Video>
+}
+
+suspend fun fetchVideo(id: Int): Video {
+    val responsePromise = window.fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
+    val response = responsePromise.await()
+    val jsonPromise = response.json()
+    val json = jsonPromise.await()
+    return json.unsafeCast<Video>()
+}
+
+/*
+Could also be chained, coroutines will still execute as expected.
+suspend fun fetchVideo(id: Int): Video =
+        window.fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
+                .await()
+                .json()
+                .await()
+                .unsafeCast<Video>()
+ */
+
+suspend fun fetchVideos(): List<Video> = coroutineScope {
+    (1..25).map { id ->
+        async {
+            fetchVideo(id)
+        }
+    }.awaitAll()
 }
