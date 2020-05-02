b0144b02a0902eef7ba00621c2777d9662468bec
Ewald Horn <ewaldhorn@gmail.com>
2020-05-02 15:10:29 +0200

    Building the Board and Square components.

--- Web/kotlin_js_tic_tac_toe/src/main/kotlin/components/Board.kt
+++ Web/kotlin_js_tic_tac_toe/src/main/kotlin/components/Board.kt
@@ -1,17 +1,35 @@
 package components
 
+import kotlinx.html.*
+import kotlinx.html.js.*
 import react.*
 import react.dom.*
 
-class Board : RComponent<RProps, RState>() {
+class Board : RComponent<BoardProps, RState>() {
     override fun RBuilder.render() {
-        div {
-            +"Board"
+        div(classes = "board") {
+            props.squares.mapIndexed { index, square ->
+                square {
+                    +"$index  $square"
+                    key = index.toString()
+                    attrs {
+                        onClickFunction = {
+                            props.onClick(index)
+                        }
+                        value = square.props.value
+                    }
+                }
+            }
         }
     }
 }
 
-fun RBuilder.board(handler: RProps.() -> Unit): ReactElement {
+external interface BoardProps: RProps {
+    var squares: List<Square>
+    var onClick: (Int) -> Unit
+}
+
+fun RBuilder.board(handler: BoardProps.() -> Unit): ReactElement {
     return child(Board::class) {
         this.attrs(handler)
     }
--- Web/kotlin_js_tic_tac_toe/src/main/kotlin/components/Square.kt
+++ Web/kotlin_js_tic_tac_toe/src/main/kotlin/components/Square.kt
@@ -1,17 +1,30 @@
 package components
 
+import kotlinx.html.js.*
 import react.*
 import react.dom.*
 
-class Square : RComponent<RProps, RState>() {
+class Square : RComponent<SquareProps, RState>() {
     override fun RBuilder.render() {
         div {
-            +"Square"
+            button(classes = "square-button") {
+                attrs {
+                    onClickFunction = {
+                        props.onClick()
+                    }
+                    +props.value
+                }
+            }
         }
     }
 }
 
-fun RBuilder.square(handler: RProps.() -> Unit): ReactElement {
+external interface SquareProps: RProps {
+    var value: String
+    var onClick: () -> Unit
+}
+
+fun RBuilder.square(handler: SquareProps.() -> Unit): ReactElement {
     return child(Square::class) {
         this.attrs(handler)
     }
