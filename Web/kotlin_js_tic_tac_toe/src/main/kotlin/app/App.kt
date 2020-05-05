0683395aaef2071fa0c4d44ea198a0fc8ae7217f
Ewald Horn <ewaldhorn@gmail.com>
2020-05-05 20:14:43 +0200

    Working on some Kotlin to JS conversions.

--- Web/kotlin_js_tic_tac_toe/src/main/kotlin/app/App.kt
+++ Web/kotlin_js_tic_tac_toe/src/main/kotlin/app/App.kt
@@ -5,6 +5,7 @@ import react.*
 
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
-        game { }
+        game {
+        }
     }
 }
--- Web/kotlin_js_tic_tac_toe/src/main/kotlin/components/Game.kt
+++ Web/kotlin_js_tic_tac_toe/src/main/kotlin/components/Game.kt
@@ -5,7 +5,7 @@ import react.dom.*
 
 class Game : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
-        div {
+        div(classes = "game") {
             +"Game"
         }
     }
@@ -16,3 +16,31 @@ fun RBuilder.game(handler: RProps.() -> Unit): ReactElement {
         this.attrs(handler)
     }
 }
+
+external interface GameState : RState {
+
+}
+
+fun calculateWinner(squares: List<Square>): String {
+    var winner = ""
+
+    for (i in 0..squares.size) {
+        val line = squares[i]
+        // if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        //       return squares[a];
+        console.log("$i  $line")
+    }
+
+    return winner
+}
+
+val lines = arrayOf(
+    intArrayOf(0, 1, 2),
+    intArrayOf(3, 4, 5),
+    intArrayOf(6, 7, 8),
+    intArrayOf(0, 3, 6),
+    intArrayOf(1, 4, 7),
+    intArrayOf(2, 5, 8),
+    intArrayOf(0, 4, 8),
+    intArrayOf(2, 4, 6)
+)
