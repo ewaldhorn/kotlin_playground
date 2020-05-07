b63753f01949b6273aad86e670a7657643c593e7
Ewald Horn <ewaldhorn@gmail.com>
2020-05-07 17:10:21 +0200

    Showcase base structure in place.

---
+++ Web/showcaser/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("org.jetbrains.kotlin.js") version "1.3.72"
+}
+
+group = "za.co.nofuss.showcaser"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-js"))
+}
+
+kotlin.target.browser { }
