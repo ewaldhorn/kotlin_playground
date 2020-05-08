f85a8b59cc199576230af1f8bfd1ad78f103227f
Ewald Horn <ewaldhorn@gmail.com>
2020-05-08 12:23:52 +0200

    Moved CSS Animation example into its own package. Stopped the CSS Animation function from being deleted by the Dead Code Eliminator (DCE).

---
+++ Web/showcaser/src/main/kotlin/animations/Animations.kt
@@ -0,0 +1,23 @@
+package animations
+
+import org.w3c.dom.*
+import kotlin.browser.*
+import kotlin.dom.*
+
+@JsName("startCSSAnimationSample")
+fun startCSSAnimationSample() {
+    clearLoadingForElement(document.getElementById("p1"), 2000)
+
+    for (i in 1..10) {
+        clearLoadingForElement(document.getElementById("l$i"),
+            2250 + kotlin.random.Random.nextInt(4000))
+    }
+}
+
+fun clearLoadingForElement(el: Element?, delay: Int) {
+    el?.let {
+        window.setTimeout({
+            it.removeClass("loading")
+        }, delay)
+    }
+}
